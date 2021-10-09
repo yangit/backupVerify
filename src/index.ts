9a3064f38b81af1f23a426d91c1141207e3fa1fb
yangit <2031974+yangit@users.noreply.github.com>
2021-10-09 20:17:14 +0800

    modify

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import './init';
+import bs58 from 'bs58';
 import fs from 'fs';
 import instanceManager from './instanceManager';
 import { setConfig } from './getConfig';
@@ -7,27 +8,26 @@ import sendMessage from './sendMessage';
 import integrityCheckExecutor from './integrityCheck';
 
 const stdin = fs.readFileSync(0).toString(); // STDIN_FILENO = 0
-const configIn = JSON.parse(stdin);
+const configIn = JSON.parse(bs58.decode(stdin.replace(/\n/g, '')).toString('utf8'));
 
 const main = async () => {
-  try {
-    setConfig(configIn);
-    const manager = instanceManager('integrity');
-    const ip = await manager.up();
-    console.log('Instance IP is', ip);
+  setConfig(configIn);
+  const manager = instanceManager('integrity');
+  const ip = await manager.up();
+  console.log('Instance IP is', ip);
 
-    const reports = await runViaSSH({
-      ip,
-      executor: integrityCheckExecutor,
-    });
-    await sendMessage(JSON.stringify(reports, null, '\t'));
-    await manager.down();
-    console.log(222);
-    process.exit();
-  } catch (err) {
-    console.log(err);
-    sendMessage(err?.message);
-    sendMessage(err?.stack);
-  }
+  const reports = await runViaSSH({
+    ip,
+    executor: integrityCheckExecutor,
+  });
+  await sendMessage(JSON.stringify(reports, null, '\t'));
+  await manager.down();
+  console.log('All done');
+  process.exit(0);
 };
-main();
+
+main().catch(async (err) => {
+  console.log(err);
+  await sendMessage('BackupVerify error. Please check logs');
+  process.exit(1);
+});
