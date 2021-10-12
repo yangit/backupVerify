8cb5dacf5c679ea8c253f6e2e25294b7c5193400
Yan <2031974+yangit@users.noreply.github.com>
2021-10-12 11:09:07 +0800

    tmux

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
-scp dist/backupVerify.js nas:~/backupVerify/
-scp dist/copySnapshots.js nas:~/backupVerify/
+scp dist/backupVerify.js backupManager:~/backupVerify/
+scp dist/copySnapshots.js backupManager:~/backupVerify/
